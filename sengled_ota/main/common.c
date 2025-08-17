421c6da93bd679664b51f3782779dfbe5a495709
Matt Falcon <falcon4@gmail.com>
2025-08-16 19:55:38 -0700

    Sengled-Tools - common.c - String helper for logs
    It's nice to see where errors occur; not so nice if you can't print them

--- sengled_ota/main/common.c
+++ sengled_ota/main/common.c
@@ -4,14 +4,20 @@
 #include "common.h"
 #include <stdlib.h>
 #include <strings.h>
+#include <stdarg.h>
 
-esp_err_t send_text(httpd_req_t* r, const char* status, const char* type, const char* body){
+esp_err_t send_text(httpd_req_t* r, const char* status, const char* type, const char* format, ...){
+    va_list args;
+    va_start(args, format);
     if (status) httpd_resp_set_status(r, status);
     if (type)   httpd_resp_set_type(r, type);
-    return httpd_resp_send(r, body, HTTPD_RESP_USE_STRLEN);
-}
-esp_err_t send_err(httpd_req_t* r, const char* status, const char* msg){
-    return send_text(r, status, "text/plain", msg);
+    int len = snprintf(0, 0, format, args);
+    va_end(args);
+    char *output = malloc(len+1);
+    va_start(args, format);
+    vsnprintf(output, len+1, format, args);
+    va_end(args);
+    return httpd_resp_send(r, output, HTTPD_RESP_USE_STRLEN);
 }
 bool query_get(httpd_req_t* r, const char* key, char* out, size_t outlen){
     size_t ql = httpd_req_get_url_query_len(r) + 1;
