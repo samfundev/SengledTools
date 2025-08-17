af7bb1ca0daf82f43db8bb1756fec96b697dd2f5
Matt Falcon <falcon4@gmail.com>
2025-08-16 19:56:44 -0700

    Sengled-Rescue - common.h - yes
    I promise some day I'll actually learn to use git properly so I don't have to commit one file at a time from Web UI

--- sengled_ota/main/common.h
+++ sengled_ota/main/common.h
@@ -10,8 +10,7 @@
 #define SECTOR_SIZE   4096
 #define RECV_CHUNK    1024
 
-esp_err_t send_text(httpd_req_t* r, const char* status, const char* type, const char* body);
-esp_err_t send_err (httpd_req_t* r, const char* status, const char* msg);
+esp_err_t send_text(httpd_req_t* r, const char* status, const char* type, const char* body, ...);
 bool      query_get(httpd_req_t* r, const char* key, char* out, size_t outlen);
 uint32_t  parse_u32_auto(const char* s); // 123 or 0x123
 static inline esp_err_t httpd_resp_sendstr_chunk(httpd_req_t *r, const char *s) {
